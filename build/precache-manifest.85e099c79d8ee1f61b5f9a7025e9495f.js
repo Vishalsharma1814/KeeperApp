fc6b1a564d9509fbd97b8c9b83c5fc5581d5a577
Vishal Sharma <vishal.sharma181418@gmail.com>
2021-05-04 15:29:38 +0530

    second  commit

---
+++ build/precache-manifest.85e099c79d8ee1f61b5f9a7025e9495f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "067112b88ded5cc5c597df1299d0b766",
+    "url": "/KeeperApp/index.html"
+  },
+  {
+    "revision": "edb34aff80942a07889b",
+    "url": "/KeeperApp/static/js/2.062b51f2.chunk.js"
+  },
+  {
+    "revision": "9407c3f765d8f9488cb4",
+    "url": "/KeeperApp/static/js/main.fea2dacf.chunk.js"
+  },
+  {
+    "revision": "ad92294fb1599dc8ff6b",
+    "url": "/KeeperApp/static/js/runtime-main.ff26de38.js"
+  }
+]);
