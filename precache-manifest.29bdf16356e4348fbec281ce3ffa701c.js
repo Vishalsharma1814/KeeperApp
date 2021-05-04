2f151706b92af5182a84f6caa8017daadf5751ed
Vishal Sharma <vishal.sharma181418@gmail.com>
2021-05-04 15:30:09 +0530

    Updates

---
+++ precache-manifest.29bdf16356e4348fbec281ce3ffa701c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d2e125278bd8cee421461976d47aa0f",
+    "url": "/KeeperApp/index.html"
+  },
+  {
+    "revision": "edb34aff80942a07889b",
+    "url": "/KeeperApp/static/js/2.062b51f2.chunk.js"
+  },
+  {
+    "revision": "9407c3f765d8f9488cb4",
+    "url": "/KeeperApp/static/js/main.fea2dacf.chunk.js"
+  },
+  {
+    "revision": "ad92294fb1599dc8ff6b",
+    "url": "/KeeperApp/static/js/runtime-main.ff26de38.js"
+  }
+]);
